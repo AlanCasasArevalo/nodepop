006a0b3f50dabf580d8be78f27d412fc15626967
Alan Casas <alancasas@MacBook-Pro-de-Alan.local>
2017-05-20 00:15:41 +0200

    Creación de archivo de configuración básica, en dicho archivo esta contenido el numero secreto de tokens creado por el desarrollador Modificación de package.json debido a instalacion de librerías Creación de archivos de autorización con JWT Modificación de archivo de usuarios para poder brindar mas seguridad a los mismos Modificación de archivos de productos para que solo puedan eliminar, actualizar o insertar los usuarios registrados. Modificación del archivo de usuarios para que usuarios nuevos puedan registrarse y usuarios ya inscritos puedan entrar a la app

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", require("./routes/index"));
-app.use("/api/products", require("./routes/api/products"));
-app.use("/api/users", require("./routes/api/users"));
+app.use("/", 				require("./routes/index"));
+app.use("/api/products", 	require("./routes/api/products"));
+app.use("/api/users", 		require("./routes/api/users"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 "use strict";
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const crypto = require("crypto");
 
 // Le decimos a mongoose que las promesas que vamos usar son globales
 mongoose.Promise = global.Promise;
@@ -22,6 +24,30 @@ const UserSchema = mongoose.Schema({
 		required: true
 	}
 });
+
+
+// Creamos una funcion para que antes de guardar el usuario se guarde las contraseñas
+UserSchema.pre("save", (next) => {
+	// Creamos el usuario
+	let user = this;
+
+	// Encriptamos la contraseña 
+	bcrypt.genSalt(10, (err, salt) => {
+		// Si hay error pasamos el error 
+		if (err) return next();
+		// si no entonces metemos la contraseña en el campo contraseña pasandole un salto y codigos de error 
+		bcrypt.hash(user.password, salt, null, (err, hash) => {
+			// Si hay error pasamos el error
+			if(err) return next(err);
+			// Si no pasamos la contraseña ya hasheada 
+			user.password = hash;
+			// Pasamos al siguiente middleware
+			next();
+		});
+	});
+});
+
+
 
 var User = mongoose.model("User", UserSchema);
 
