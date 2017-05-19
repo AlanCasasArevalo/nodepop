b5ab35df07252e474f61d8288d5dcaa324fcb285
Alan Casas <alancasas@MacBook-Pro-de-Alan.local>
2017-05-19 10:14:17 +0200

    Modificación del archivo app.js para que al entrar por el navegador se entre desde /api/products, modificación para que le llegue el mensaje si es una api o no en html Modificación del archivo products.js en la carpeta routes/api para que se puedan publicar productos desde el body (POST) Eliminación de archivo users, debido a que no va a ser utilizado

--- routes/api/products.js
+++ routes/api/products.js
@@ -4,22 +4,33 @@ const express = require("express");
 const api = express.Router();
 const Product = require("../../model/Product");
 
-api.get("/products", (req, res, next) => {
+api.get("/", (req, res, next) => {
     
 	Product.find().exec((err, products) => {
 		if(err){
 			next(err);
 			return;
 		}
-
-
-
 		res.json({
 			success:true,
 			result:products
 		});
-
 	});   
+});
+
+api.post("/", (req, res,next) => {
+	console.log(req.body);
+	const product = new Product(req.body);
+	product.save((err, productSaved) => {
+		if (err){
+			next(err);
+			return;
+		}
+		res.json({
+			success:true,
+			result: productSaved
+		});
+	});
 });
 
 
